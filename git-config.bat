fa2b5d58a631ee24745db13a64040b4113c00ba2
Younes Moudaoui <younes.moudaoui@gmail.com>
2025-06-19 10:19:00 +0100

    Remove batch files

--- git-config.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Setting up Git user configuration...
-git config --global user.email "younes.moudaoui@gmail.com"
-git config --global user.name "Younes Moudaoui"
-echo Done!
-pause 
--- git-setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Setting up Git repository...
-git remote remove origin
-git remote add origin https://github.com/YounesMoudaoui/web4jobsbackendj.git
-git add .
-git commit -m "Initial commit"
-git push -u origin master
-echo Done!
-pause 
